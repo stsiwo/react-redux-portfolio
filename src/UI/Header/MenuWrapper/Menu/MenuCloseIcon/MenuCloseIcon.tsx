5af07dc4ee77faea31e1aa9d2ef652a27126d316
sts-ubuntu <stsiwo@gmail.com>
2019-09-27 04:20:47 -0700

    APOLLO: fixed minor issue

--- src/UI/Header/MenuWrapper/Menu/MenuCloseIcon/MenuCloseIcon.tsx
+++ src/UI/Header/MenuWrapper/Menu/MenuCloseIcon/MenuCloseIcon.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './MenuCloseIcon.scss';
 import { useToggleMenuSidebarMutation } from '../../../../Base/Hooks/Mutations/useToggleMuneSidebarMutation';
+import { useCssGlobalContext } from '../../../../Base/Context/CssGlobalContext/CssGlobalContext';
+import { useResponsiveComponent } from '../../../../Base/Hooks/ResponsiveComponentHook';
 
 
 const MenuCloseIcon: React.FunctionComponent<{}> = (props: {}) => {
@@ -11,10 +13,15 @@ const MenuCloseIcon: React.FunctionComponent<{}> = (props: {}) => {
     toggleMenuSidebar();
   }
 
+  const globalCss = useCssGlobalContext();
+  const currentScreenWidth = useResponsiveComponent();
+
   return (
+    currentScreenWidth < globalCss.laptopSize && (
     <i className="regular-icon header-menu-close-icon" onClick={handleToggleMenuSidebar}>
       &#10006;
     </i>
+    )
   );
 }
 
